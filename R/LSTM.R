58862ad20e347d5a9fa8ca74831ea4b92f1b0a8e
alvinlui99 <78842323+alvinlui99@users.noreply.github.com>
2024-08-15 14:50:57 +0800

    linting

--- R/LSTM.R
+++ R/LSTM.R
@@ -6,28 +6,29 @@ library(tensorflow)
 
 source("util_functions.R")
 
-csv_folder <- 'Binance Data/'
-symbol = 'BTCUSDT'
+csv_folder <- "Binance Data/"
+symbol <- "BTCUSDT"
 
-csv_path <- paste0(csv_folder, symbol, '.csv')
-OHLCV <- read.csv(csv_path, colClasses = c("index" = "POSIXct"))
+csv_path <- paste0(csv_folder, symbol, ".csv")
+ohlcv_data <- read.csv(csv_path, colClasses = c("index" = "POSIXct"))
 
-OHLCV <- feature_generation(OHLCV)
+ohlcv_data <- feature_generation(ohlcv_data)
 
-OHLCV_ind <- OHLCV %>%
+ohlcv_data_ind <- ohlcv_data %>%
   select(starts_with("ind_"))
 
-pca <- OHLCV_ind %>%
+pca <- ohlcv_data_ind %>%
   na.omit() %>%
   scale() %>%
   prcomp()
 
-input_X <- pca$rotation[, 1:8]
+x_train <- pca$rotation[, 1:8]
 
 
-# Reshape input_X for LSTM model (assuming a time series sequence)
+# Reshape x_train for LSTM model (assuming a time series sequence)
 # Reshape the data into [samples, time steps, features] for LSTM
-input_X <- array(input_X, dim = c(dim(input_X)[1], 1, dim(input_X)[2]))
+x_train <- array(x_train,
+                 dim = c(dim(x_train)[1], 1, dim(x_train)[2]))
 
 # Build the LSTM model
 model <- keras_model_sequential()
@@ -37,23 +38,22 @@ model %>%
 
 # Compile the model
 model %>% compile(
-  loss = 'mean_squared_error',
+  loss = "mean_squared_error",
   optimizer = optimizer_adam()
 )
 
 # Train the LSTM model (Assuming you have target data y_train)
 model %>% fit(
-  input_X, y_train,
+  x_train, y_train,
   epochs = 100,
   batch_size = 32
 )
 
 # Make predictions using the model
-predictions <- model %>% predict(input_X)
+predictions <- model %>% predict(x_train)
 
 # Print predictions
 print(predictions)
 
-
-
-
+length(x_train)
+length(predictions)
--- R/util_functions.R
+++ R/util_functions.R
@@ -1,264 +1,268 @@
-get_signal <- function(OHLCV) {
-  signal <- rep(0, nrow(OHLCV))  # Initialize the signal vector with zeros
-  
+library(TTR)
+library(dplyr)
+
+get_signal <- function(ohlcv_data) {
+  signal <- rep(0, nrow(ohlcv_data))  # Initialize the signal vector with zeros
   # Check the conditions for the signal
-  buy_signal <- 
-    lag(OHLCV$sma14) < lag(OHLCV$sma200) &
-    OHLCV$sma14 > OHLCV$sma200
+  buy_signal <-
+    lag(ohlcv_data$sma14) < lag(ohlcv_data$sma200) &
+    ohlcv_data$sma14 > ohlcv_data$sma200
   sell_signal <-
-    lag(OHLCV$sma14) > lag(OHLCV$sma200) &
-    OHLCV$sma14 < OHLCV$sma200
-  
+    lag(ohlcv_data$sma14) > lag(ohlcv_data$sma200) &
+    ohlcv_data$sma14 < ohlcv_data$sma200
   signal[buy_signal] <- 1  # Set buy signal where conditions are met
   signal[sell_signal] <- -1  # Set sell signal where conditions are met
-  
   return(signal)
 }
 
-close_position <- function(OHLCV, i, price){
-  # i is the index (row) position of OHLCV
-  
+close_position <- function(ohlcv_data, i, price) {
+  # i is the index (row) position of ohlcv_data
   # ignore cost when close position
-  transfer <- OHLCV$Quantity_BOP[i] * price
-  OHLCV$Quantity_EOP[i] <- 0
-  OHLCV$Cash[i] <- OHLCV$Cash[i] + transfer
-  
-  return(OHLCV)
+  transfer <- ohlcv_data$Quantity_BOP[i] * price
+  ohlcv_data$Quantity_EOP[i] <- 0
+  ohlcv_data$Cash[i] <- ohlcv_data$Cash[i] + transfer
+  return(ohlcv_data)
 }
 
-open_position <- function(OHLCV, i = integer(), price,
-    tp = numeric(), sl = numeric(), size = numeric(1),
-    commission = numeric(0)){
+open_position <- function(ohlcv_data,
+                          i = integer(),
+                          price,
+                          tp = numeric(),
+                          sl = numeric(),
+                          size = numeric(1),
+                          commission = numeric(0)) {
   # allow fractional qty
-  budget <- OHLCV$Cash[i] * size
+  budget <- ohlcv_data$Cash[i] * size
   qty <- budget / (price * (1 + commission))
-  
-  OHLCV$Cash[i] <- OHLCV$Cash[i] - budget
-  OHLCV$Quantity_EOP[i] <- OHLCV$Quantity_EOP[i] + qty
-  OHLCV$Take_Profit[i] <- tp
-  OHLCV$Stop_Loss[i] <- sl
+  ohlcv_data$Cash[i] <- ohlcv_data$Cash[i] - budget
+  ohlcv_data$Quantity_EOP[i] <- ohlcv_data$Quantity_EOP[i] + qty
+  ohlcv_data$Take_Profit[i] <- tp
+  ohlcv_data$Stop_Loss[i] <- sl
 }
 
-check_take_profit <- function(OHLCV, i, status){
+check_take_profit <- function(ohlcv_data, i, status) {
   if (is.na(status$tp)) {
-    return (FALSE)
-  } else if (status$qty > 0 & OHLCV$High[i] > status$tp) {
-    return (TRUE)
-  } else if (status$qty < 0 & OHLCV$Low[i] < status$tp) {
-    return (TRUE)
+    return(FALSE)
+  } else if (status$qty > 0 && ohlcv_data$High[i] > status$tp) {
+    return(TRUE)
+  } else if (status$qty < 0 && ohlcv_data$Low[i] < status$tp) {
+    return(TRUE)
   }
-  return (FALSE)
+  return(FALSE)
 }
 
-check_stop_loss <- function(OHLCV, i, status){
+check_stop_loss <- function(ohlcv_data, i, status) {
   if (is.na(status$sl)) {
-    return (FALSE)
-  } else if (status$qty > 0 & OHLCV$Low[i] < status$sl) {
-    return (TRUE)
-  } else if (status$qty < 0 & OHLCV$High[i] > status$sl) {
-    return (TRUE)
+    return(FALSE)
+  } else if (status$qty > 0 && ohlcv_data$Low[i] < status$sl) {
+    return(TRUE)
+  } else if (status$qty < 0 && ohlcv_data$High[i] > status$sl) {
+    return(TRUE)
   }
-  return (FALSE)
+  return(FALSE)
 }
 
-execute_take_profit <- function(OHLCV, i, status){
-  OHLCV$Cash[i:nrow(OHLCV)] <- OHLCV$Cash[i] + status$qty * status$tp
+execute_take_profit <- function(ohlcv_data, i, status) {
+  ohlcv_data$Cash[i:nrow(ohlcv_data)] <-
+    ohlcv_data$Cash[i] + status$qty * status$tp
   status$qty <- 0
   status$tp <- status$sl <- NA
-  OHLCV$qty[i:nrow(OHLCV)] <- rep(status$qty, nrow(OHLCV) - i + 1)
-  
-  return(list(OHLCV = OHLCV, status = status))
+  ohlcv_data$qty[i:nrow(ohlcv_data)] <-
+    rep(status$qty, nrow(ohlcv_data) - i + 1)
+  return(list(ohlcv_data = ohlcv_data, status = status))
 }
 
-execute_stop_loss <- function(OHLCV, i, status){
-  OHLCV$Cash[i:nrow(OHLCV)] <- OHLCV$Cash[i] + status$qty * status$sl
+execute_stop_loss <- function(ohlcv_data, i, status) {
+  ohlcv_data$Cash[i:nrow(ohlcv_data)] <-
+    ohlcv_data$Cash[i] + status$qty * status$sl
   status$qty <- 0
   status$tp <- status$sl <- NA
-  OHLCV$qty[i:nrow(OHLCV)] <- rep(status$qty, nrow(OHLCV) - i + 1)
-  
-  return(list(OHLCV = OHLCV, status = status))
+  ohlcv_data$qty[i:nrow(ohlcv_data)] <-
+    rep(status$qty, nrow(ohlcv_data) - i + 1)
+  return(list(ohlcv_data = ohlcv_data, status = status))
 }
 
-backtesting <- function(OHLCV, params){
+backtesting <- function(ohlcv_data, params) {
   # Initialize columns with zeros----
-  OHLCV$Cash <- rep(params$cash, nrow(OHLCV))
-  
-  OHLCV$qty <- 
-    OHLCV$AV <- 
-    rep(0, nrow(OHLCV))
-  
+  ohlcv_data$Cash <- rep(params$cash, nrow(ohlcv_data))
+
+  ohlcv_data$qty <-
+    ohlcv_data$AV <-
+    rep(0, nrow(ohlcv_data))
+
   status <- list(
     active_order = "none",
     qty = 0,
     tp = NA,
     sl = NA
   )
-  
-  # Loop through the OHLCV data----
-  for (i in 1:nrow(OHLCV)) {  # Start from the 201st row to ensure enough data for SMAs
+
+  # Loop through the ohlcv_data data----
+  for (i in seq_len(nrow(ohlcv_data))) {
     # First check if hit take profit or stop loss
     # Then check if there is active order
     # Then check signal to create order
-    
-    
+
     ## Take Profit or Stop Loss----
     # Check if hit take profit or stop loss
-    
-    if (check_take_profit(OHLCV, i, status)){
-      temp <- execute_take_profit(OHLCV, i, status)
-      OHLCV <- temp$OHLCV
+
+    if (check_take_profit(ohlcv_data, i, status)) {
+      temp <- execute_take_profit(ohlcv_data, i, status)
+      ohlcv_data <- temp$ohlcv_data
       status <- temp$status
     }
-    
-    if (check_stop_loss(OHLCV, i, status)){
-      temp <- execute_stop_loss(OHLCV, i, status)
-      OHLCV <- temp$OHLCV
+
+    if (check_stop_loss(ohlcv_data, i, status)) {
+      temp <- execute_stop_loss(ohlcv_data, i, status)
+      ohlcv_data <- temp$ohlcv_data
       status <- temp$status
     }
-    
+
     # Check if opposite signal
-    
+
     # Long position
-    if (status$qty > 0){
-      if(OHLCV$Signal[i] == -1){  # Short signal
+    if (status$qty > 0) {
+      if (ohlcv_data$Signal[i] == -1) {  # Short signal
         # Close position with Close price
-        OHLCV$Cash[i:nrow(OHLCV)] <- OHLCV$Cash[i] + status$qty * Cl(OHLCV)[i]  
+        ohlcv_data$Cash[i:nrow(ohlcv_data)] <-
+          ohlcv_data$Cash[i] + status$qty * Cl(ohlcv_data)[i]
         status$qty <- 0
         status$tp <- status$sl <- NA
-        OHLCV$qty[i:nrow(OHLCV)] <- 0
+        ohlcv_data$qty[i:nrow(ohlcv_data)] <- 0
       }
     }
-    
+
     # Short position
-    if (status$qty < 0){  
-      if(OHLCV$Signal[i] == 1){  # Short signal
+    if (status$qty < 0) {
+      if (ohlcv_data$Signal[i] == 1) {  # Short signal
         # Close position with Close price
-        OHLCV$Cash[i:nrow(OHLCV)] <- OHLCV$Cash[i] + status$qty * Cl(OHLCV)[i]  
+        ohlcv_data$Cash[i:nrow(ohlcv_data)] <-
+          ohlcv_data$Cash[i] + status$qty * Cl(ohlcv_data)[i]
         status$qty <- 0
         status$tp <- status$sl <- NA
-        OHLCV$qty[i:nrow(OHLCV)] <- 0
+        ohlcv_data$qty[i:nrow(ohlcv_data)] <- 0
       }
     }
-    
+
     ## Execute order----
-    
+
     # Execute Long Market Order
-    if (status$active_order == "Long Market Order"){
-      entry_price <- OHLCV$Open[i]
+    if (status$active_order == "Long Market Order") {
+      entry_price <- ohlcv_data$Open[i]
       status$tp <- entry_price * (1 + params$take_profit_pc)
       status$sl <- entry_price * (1 - params$stop_loss_pc)
-      
-      budget <- OHLCV$Cash[i] * params$order_size
+
+      budget <- ohlcv_data$Cash[i] * params$order_size
       status$qty <- budget / (entry_price * (1 + params$commission))
-      
-      cash <- OHLCV$Cash[i] - budget
-      OHLCV$Cash[i:nrow(OHLCV)] <- cash
-      OHLCV$qty[i:nrow(OHLCV)] <- status$qty
-      
+
+      cash <- ohlcv_data$Cash[i] - budget
+      ohlcv_data$Cash[i:nrow(ohlcv_data)] <- cash
+      ohlcv_data$qty[i:nrow(ohlcv_data)] <- status$qty
+
       status$active_order <- "none"
     }
-    
+
     # Execute Short Market Order
-    if (status$active_order == "Short Market Order"){
-      entry_price <- OHLCV$Open[i]
+    if (status$active_order == "Short Market Order") {
+      entry_price <- ohlcv_data$Open[i]
       status$tp <- entry_price * (1 - params$take_profit_pc)
       status$sl <- entry_price * (1 + params$stop_loss_pc)
-      
-      budget <- OHLCV$Cash[i] * params$order_size
+
+      budget <- ohlcv_data$Cash[i] * params$order_size
       status$qty <- - budget / (entry_price * (1 + params$commission))
-      
-      cash <- OHLCV$Cash[i] + budget
-      OHLCV$Cash[i:nrow(OHLCV)] <- cash
-      OHLCV$qty[i:nrow(OHLCV)] <- status$qty
-      
+
+      cash <- ohlcv_data$Cash[i] + budget
+      ohlcv_data$Cash[i:nrow(ohlcv_data)] <- cash
+      ohlcv_data$qty[i:nrow(ohlcv_data)] <- status$qty
+
       status$active_order <- "none"
     }
-    
-    
+
+
     ## Create order----
-    
+
     # Create Long Market Order
-    if (OHLCV$Signal[i] == 1 & status$qty == 0) {
+    if (ohlcv_data$Signal[i] == 1 && status$qty == 0) {
       status$active_order <- "Long Market Order"
     }
-    
+
     # Create Short Market Order
-    if (OHLCV$Signal[i] == -1 & status$qty == 0) {
+    if (ohlcv_data$Signal[i] == -1 && status$qty == 0) {
       status$active_order <- "Short Market Order"
     }
-    
   }
-  
+
   # Calculate Account Value----
-  
-  OHLCV$AV <- OHLCV$Cash + OHLCV$qty * Cl(OHLCV)
-  
-  return (OHLCV)
+
+  ohlcv_data$AV <- ohlcv_data$Cash + ohlcv_data$qty * Cl(ohlcv_data)
+
+  return(ohlcv_data)
 }
 
-evaluate_strategy <- function(OHLCV){
-  total_days <- as.numeric(OHLCV$index[nrow(OHLCV)] - OHLCV$index[1],
-                           units = "days")
-  Return_BnH <- (OHLCV$Close[nrow(OHLCV)] / OHLCV$Close[1]) ^
+evaluate_strategy <- function(ohlcv_data) {
+  total_days <-
+    as.numeric(ohlcv_data$index[nrow(ohlcv_data)] - ohlcv_data$index[1],
+               units = "days")
+  return_buy_and_hold <-
+    (ohlcv_data$Close[nrow(ohlcv_data)] / ohlcv_data$Close[1]) ^
     (365 / total_days)
-  Return_Strategy <- (OHLCV$AV[nrow(OHLCV)] / OHLCV$AV[1]) ^
+  return_strategy <- (ohlcv_data$AV[nrow(ohlcv_data)] / ohlcv_data$AV[1]) ^
     (365 / total_days)
-  Premium <- Return_Strategy - Return_BnH
-  
-  return(Premium)
+  premium <- return_strategy - return_buy_and_hold
+
+  return(premium)
 }
 
-feature_generation <- function(OHLCV){
+feature_generation <- function(ohlcv_data) {
   # MACD----
-  MACD_df <- as.data.frame(MACD(Cl(OHLCV)))
-  names(MACD_df) <- paste0("ind_MACD_", names(MACD_df))
-  OHLCV <- cbind(OHLCV, MACD_df)
-  
+  macd_df <- as.data.frame(MACD(Cl(ohlcv_data)))
+  names(macd_df) <- paste0("ind_MACD_", names(macd_df))
+  ohlcv_data <- cbind(ohlcv_data, macd_df)
+
   # RSI----
-  OHLCV$ind_RSI <- RSI(Cl(OHLCV))
-  
+  ohlcv_data$ind_RSI <- RSI(Cl(ohlcv_data))
+
   # Moving Average----
-  for (window in seq(20, 200, by=20)){
-    sma <- SMA(Cl(OHLCV), window)
-    OHLCV[[paste0("ind_SMA", window)]] <- sma / Cl(OHLCV)
-    
-    ema <- EMA(Cl(OHLCV), window)
-    OHLCV[[paste0("ind_EMA", window)]] <- ema / Cl(OHLCV)
+  for (window in seq(20, 200, by = 20)) {
+    sma <- SMA(Cl(ohlcv_data), window)
+    ohlcv_data[[paste0("ind_SMA", window)]] <- sma / Cl(ohlcv_data)
+
+    ema <- EMA(Cl(ohlcv_data), window)
+    ohlcv_data[[paste0("ind_EMA", window)]] <- ema / Cl(ohlcv_data)
   }
-  
+
   # Bollinger Bands----
-  BBands_df <- as.data.frame(BBands(OHLCV[,c("High","Low","Close")]))
-  names(BBands_df) <- paste0("ind_BBands_", names(BBands_df))
-  OHLCV <- cbind(OHLCV, BBands_df)
-  BBands_col_names_relative <- c("ind_BBands_dn", "ind_BBands_mavg", "ind_BBands_up")
-  OHLCV[, c(BBands_col_names_relative, "Close")] <- OHLCV %>%
-    select(all_of(c(BBands_col_names_relative, "Close"))) %>%
-    mutate(across(all_of(BBands_col_names_relative), ~./Close))
-  
+  bbands_df <- as.data.frame(BBands(ohlcv_data[, c("High", "Low", "Close")]))
+  names(bbands_df) <- paste0("ind_BBands_", names(bbands_df))
+  ohlcv_data <- cbind(ohlcv_data, bbands_df)
+  bbands_col_names_relative <-
+    c("ind_BBands_dn", "ind_BBands_mavg", "ind_BBands_up")
+  ohlcv_data[, c(bbands_col_names_relative, "Close")] <- ohlcv_data %>%
+    select(all_of(c(bbands_col_names_relative, "Close"))) %>%
+    mutate(across(all_of(bbands_col_names_relative), ~. / Close))
+
   # Welles Wilder’s Directional Movement Index----
-  ADX_df <- as.data.frame(ADX(OHLCV[,c("High","Low","Close")]))
-  names(ADX_df) <- paste0("ind_ADX_", names(ADX_df))
-  OHLCV <- cbind(OHLCV, ADX_df)
-  
+  adx_df <- as.data.frame(ADX(ohlcv_data[, c("High", "Low", "Close")]))
+  names(adx_df) <- paste0("ind_ADX_", names(adx_df))
+  ohlcv_data <- cbind(ohlcv_data, adx_df)
+
   # Aroon----
-  aroon_df <- as.data.frame(aroon(OHLCV[,c("High","Low")]))
+  aroon_df <- as.data.frame(aroon(ohlcv_data[, c("High", "Low")]))
   names(aroon_df) <- paste0("ind_aroon_", names(aroon_df))
-  OHLCV <- cbind(OHLCV, aroon_df)
-  
+  ohlcv_data <- cbind(ohlcv_data, aroon_df)
+
   # Commodity Channel Index----
-  CCI_df <- as.data.frame(CCI(OHLCV[,c("High","Low","Close")]))
-  names(CCI_df) <- paste0("ind_CCI_", names(CCI_df))
-  OHLCV <- cbind(OHLCV, CCI_df)
-  
-  
+  cci_df <- as.data.frame(CCI(ohlcv_data[, c("High", "Low", "Close")]))
+  names(cci_df) <- paste0("ind_CCI_", names(cci_df))
+  ohlcv_data <- cbind(ohlcv_data, cci_df)
+
   # Volume----
-  OHLCV$ind_Volume <- OHLCV$Volume
-  
+  ohlcv_data$ind_Volume <- ohlcv_data$Volume
+
   # Return----
-  OHLCV$ind_return <- c(NA, OHLCV$Close[-1] / OHLCV$Close[-nrow(OHLCV)])
-  
-  return(OHLCV)
+  ohlcv_data$ind_return <-
+    c(NA, ohlcv_data$Close[-1] / ohlcv_data$Close[-nrow(ohlcv_data)])
+
+  return(ohlcv_data)
 }
-
-
 
